b9d0787cdf918bd69b67fc25ea11186424bf3b65
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-10-26 01:58:55 -0230

    update to homepage

--- sprint1/src/components/HomePage.jsx
+++ sprint1/src/components/HomePage.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
-import './../styles/homepage.css';
-import pumpkin from './../images/monster_pumpkin_halloween_vector_illustration.jpg'
+import React from "react";
+import "./../styles/homepage.css";
+import pumpkin from "./../images/monster_pumpkin_halloween_vector_illustration.jpg";
 
 const HomePage = () => {
   return (
     <>
-    <figure className='flex-figure'>
-            <img className='logo' src={pumpkin} alt="Pumpkin" />
-            <figcaption>
-            <a href="https://www.freepik.com/free-vector/monster-pumpkin-halloween-vector-illustration_16628717.htm#query=evil%20pumpkin&position=1&from_view=search&track=ais">Image by inksyndromeartwork</a> on Freepik
-            </figcaption>
-    </figure>
+    <h1>REACT-O-WEEN</h1>
+      <div className="flex-figure">
+        <figure>
+          <img className="logo" src={pumpkin} alt="Pumpkin" />
+          <figcaption>
+            <a href="https://www.freepik.com/free-vector/monster-pumpkin-halloween-vector-illustration_16628717.htm#query=evil%20pumpkin&position=1&from_view=search&track=ais">
+              Image by inksyndromeartwork
+            </a>
+            on Freepik
+          </figcaption>
+        </figure>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
