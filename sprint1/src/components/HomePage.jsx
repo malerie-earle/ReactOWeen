dda8fe300a0e619f5e76c49e0980bca75aadaaa0
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-10-26 13:53:29 -0230

    Started homepage component

--- sprint1/src/components/HomePage.jsx
+++ sprint1/src/components/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./../styles/homepage.css";
 import pumpkin from "./../images/monster_pumpkin_halloween_vector_illustration.jpg";
+import banner from "./../images/coollogo_com-279531859.png";
 
 const HomePage = () => {
   return (
     <>
-    <h1>REACT-O-WEEN</h1>
+      <div className="flex-banner">
+        <img src={banner} alt="banner" />
+      </div>
       <div className="flex-figure">
         <figure>
           <img className="logo" src={pumpkin} alt="Pumpkin" />
@@ -17,6 +20,7 @@ const HomePage = () => {
           </figcaption>
         </figure>
       </div>
+      <footer></footer>
     </>
   );
 };
