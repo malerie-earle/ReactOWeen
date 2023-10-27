dd4d5fce0e12eb506c15565125e6b6e6cb48c8fc
RyanGuinchard <ryanguinchard9@hotmail.com>
2023-10-27 20:22:43 -0230

    Added navigation

--- sprint1/src/App.js
+++ sprint1/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import HomePage from "./components/HomePage";
 import SnakeGame from "./components/SnakeGame";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <HomePage></HomePage>
-      <SnakeGame></SnakeGame>
-    </>
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<HomePage />} />
+          <Route path="/snakeGame" element={<SnakeGame />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- sprint1/src/components/HomePage.jsx
+++ sprint1/src/components/HomePage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./../styles/homepage.css";
 import pumpkin from "./../images/monster_pumpkin_halloween_vector_illustration.jpg";
 import banner from "./../images/coollogo_com-279531859.png";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
@@ -9,14 +10,26 @@ const HomePage = () => {
       <div className="flex-banner">
         <img src={banner} alt="banner" />
       </div>
+      <div className="flex-icons">
+        <Link to="/snakeGame">
+          <div className="icon">
+            <span>Snake Game</span>
+          </div>
+        </Link>
+        <div className="icon">
+          <span>Costume Contest</span>
+        </div>
+        <div className="icon">
+          <span>Contact Us</span>
+        </div>
+      </div>
       <div className="flex-figure">
         <figure>
           <img className="logo" src={pumpkin} alt="Pumpkin" />
           <figcaption>
             <a href="https://www.freepik.com/free-vector/monster-pumpkin-halloween-vector-illustration_16628717.htm#query=evil%20pumpkin&position=1&from_view=search&track=ais">
-              Image by inksyndromeartwork
+              Image by inksyndromeartwork on Freepik
             </a>
-            on Freepik
           </figcaption>
         </figure>
       </div>
